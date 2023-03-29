abd6eeabe5aae4fa9c3e6e90da2d43c5df5bbdb9
Student <Student@5CD30199XL.control.ad.sit.kmutt.ac.th>
2023-03-29 11:57:27 +0700

    Exception1Finish

--- src/exception1/LAB4.java
+++ src/exception1/LAB4.java
@@ -1,21 +1,22 @@
 
 package exception1;
 
-
-public class LAB3 {
-
+public class LAB4 {
     public static void main(String[] args) {
         try {
             System.out.println("Statement1");
             System.out.println(1/0);
             System.out.println("Statement3");   
-        } 
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+        }
         catch (ArithmeticException e) {
-        } 
+            throw new StringIndexOutOfBoundsException();
+        }
+        
         finally {
             System.out.println("Statement 4");
         }
         System.out.println("statement 5");
-        
     }
 }
